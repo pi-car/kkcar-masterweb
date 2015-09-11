8ca91f42c965f6a247b3b555fa7c74437ea28f84
Garikk <supergarikk@gmail.com>
2015-09-11 20:18:59 +0300

    new diagram drawer

--- application/views/configuration_view.php
+++ application/views/configuration_view.php
@@ -1,7 +1,6 @@
 <script src="/js/kk.config.js" type="text/javascript"></script>
 <link rel="stylesheet" href="/js/jointjs/joint.css" />
 <script src="/js/jointjs/joint.min.js"></script>
-<script src="/js/jointjs/joint.shapes.devs.min.js"></script>
 
 <script type="text/javascript">
     $(document).ready(function () {
@@ -12,12 +11,35 @@
 </div>
 
 <div id="kk_config_main">
-            <div id="kk_config_config_features">
-                <select id="kk_config_configuration_features_selector" onchange="LoadFeatureToEditor(this.selectedIndex)"></select>
-            </div>
-    <div id="kk_config_config_editor">
-        
+    <div id="kk_config_config_features">
+        <select id="kk_config_configuration_features_selector" onchange="LoadFeatureToEditor(this.selectedIndex)"></select>
+        <button id="kk_config_configuration_button_save" >Сохранить</button>
+        <button id="kk_config_configuration_button_refresh" onclick="LoadCurrentFeatureToEditor()">Обновить</button>
     </div>
-    
+    <div id="kk_config_plugin_editor_overlay" class="popup_overlay">
+        <div id="kk_config_plugin_editor_linkprop" class="popup">
+            Плагин:
+            <h3 id="kk_config_plugineditor_pluginname">plugin_name</h2>
+                <h4 id="kk_config_plugineditor_plugindescription">plugin_description</h2>
+                    Соединения:
+                    <div id="kk_config_plugineditor_connections">
+                        <div id="kk_config_plugineditor_connection">
+                            <p class="pconnection_name">conn</p>
+                            <p class="pconnection_targetplugin">conn_pl</p>
+                            <ul class="pconnection_targetpins">
+                            </ul>
+                        </div>
+                    </div>
 
-</div>
+
+                    <select id="kk_config_plugin_editor_linkprop_linktype"></select>
+                    <button onclick="CloseLinkProperty()">Ok</button>
+                    </div>
+                    </div>
+
+                    <div id="kk_config_plugin_editor">
+
+                    </div>
+
+
+                    </div>
--- js/kk.config.js
+++ js/kk.config.js
@@ -4,15 +4,17 @@
  * and open the template in the editor.
  */
 
-var kk_conf_configurations_raw;
 var kk_conf_configurations_obj;
 var kk_conf_configuration_mainconf;
+var kk_conf_configuration_currentfeature;
 
 var kk_conf_confeditor_plugins_info;
 var kk_conf_confeditor_plugins_rects;
 
 var kk_conf_featureslist_id = "#kk_config_configuration_features_selector";
-var kk_conf_configurationblock = "#kk_config_config_editor";
+var kk_conf_configurationblock = "#kk_config_plugin_editor";
+var kk_conf_plugineditor_pname = "#kk_config_plugineditor_pluginname";
+var kk_conf_plugineditor_pdesc = "#kk_config_plugineditor_plugindescription";
 
 var graph;
 
@@ -42,17 +44,25 @@ function RequestConfigurationToEditor()
 
 function FillPluginsList(Data)
 {
-    kk_conf_confeditor_plugins_info=[];
-    for (var i=0;i<Data.length;i++)
+    kk_conf_confeditor_plugins_info = [];
+    for (var i = 0; i < Data.length; i++)
     {
-        if (Data[i].pins_in!==null)
-            Data[i].pins_in=JSON.parse(Data[i].pins_in).pins;
-        
-        if (Data[i].pins_out!==null)
-            Data[i].pins_out=JSON.parse(Data[i].pins_out).pins;
-        
-        kk_conf_confeditor_plugins_info[Data[i].uuid]=Data[i];
-        
+        if (Data[i].pins_in !== null)
+        {
+            Data[i].pins_in = JSON.parse(Data[i].pins_in).pins;
+            for (var ii = 0; ii < Data[i].pins_in.length; ii++)
+            {
+                Data[i].pins_in[ii] = Data[i].pins_in[ii] + "_in";
+            }
+        }
+
+        if (Data[i].pins_out !== null)
+        {
+            Data[i].pins_out = JSON.parse(Data[i].pins_out).pins;
+        }
+
+        kk_conf_confeditor_plugins_info[Data[i].uuid] = Data[i];
+
     }
 }
 
@@ -82,9 +92,24 @@ function LoadConfigurationToEditor(Data)
     }
 }
 
+
+function SaveCurrentConfiguration()
+{
+
+}
+
+function LoadCurrentFeatureToEditor()
+{
+    LoadFeatureToEditor($("#kk_config_configuration_features_selector")[0].selectedIndex);
+}
+
+
 function LoadFeatureToEditor(FeatureIndex)
 {
     var FeatureToLoad = kk_conf_configurations_obj[kk_conf_configuration_mainconf].Features[FeatureIndex];
+    
+    kk_conf_configuration_currentfeature=FeatureIndex;
+    
     kk_conf_confeditor_plugins_rects = [];
     //clear diagram
     // $(kk_conf_configurationblock).empty();
@@ -94,52 +119,59 @@ function LoadFeatureToEditor(FeatureIndex)
     {
         if (kk_conf_confeditor_plugins_rects[FeatureToLoad.Connections[i].SourcePluginUID] === undefined)
         {
-            Rect=AddDiagramBlock(FeatureToLoad.Connections[i].SourcePluginUID,FeatureToLoad.Connections[i].PinName);
+            Rect = AddDiagramBlock(FeatureToLoad.Connections[i].SourcePluginUID, FeatureToLoad.Connections[i].PinName);
             kk_conf_confeditor_plugins_rects[FeatureToLoad.Connections[i].SourcePluginUID] = Rect;
-            
+
         }
         if (kk_conf_confeditor_plugins_rects[FeatureToLoad.Connections[i].TargetPluginUID] === undefined)
         {
-            Rect=AddDiagramBlock(FeatureToLoad.Connections[i].TargetPluginUID,FeatureToLoad.Connections[i].PinName);
+            Rect = AddDiagramBlock(FeatureToLoad.Connections[i].TargetPluginUID, FeatureToLoad.Connections[i].PinName);
             kk_conf_confeditor_plugins_rects[FeatureToLoad.Connections[i].TargetPluginUID] = Rect;
         }
     }
     for (var i = 0; i < FeatureToLoad.Connections.length; i++)
     {
-        var Conn=FeatureToLoad.Connections[i];
-        for (var ii=0; ii<Conn.PinName.length;ii++)
-        {
-            AddDiagramLink(kk_conf_confeditor_plugins_rects[Conn.SourcePluginUID].id,kk_conf_confeditor_plugins_rects[Conn.SourcePluginUID].ports[Conn.PinName[ii]],kk_conf_confeditor_plugins_rects[Conn.TargetPluginUID].id,kk_conf_confeditor_plugins_rects[Conn.TargetPluginUID].ports[Conn.PinName[ii]]);
-        }
+        var Conn = FeatureToLoad.Connections[i];
+        AddDiagramLink(kk_conf_confeditor_plugins_rects[Conn.SourcePluginUID].id, kk_conf_confeditor_plugins_rects[Conn.TargetPluginUID].id, Conn.ConnectionName);
     }
 
 }
 
 function AddDiagramBlock(id)
 {
-    
-    var rect = new joint.shapes.devs.Model({
+
+    var rect = new joint.shapes.basic.Rect({
         size: {width: 120, height: 90},
-        inPorts: kk_conf_confeditor_plugins_info[id].pins_in,
-        outPorts: kk_conf_confeditor_plugins_info[id].pins_out,
         attrs: {
-        '.label': { text: kk_conf_confeditor_plugins_info[id].name, 'ref-x': .4, 'ref-y': .2 },
-        rect: { fill: '#2ECC71' },
-        '.inPorts circle': { fill: '#16A085' },
-        '.outPorts circle': { fill: '#E74C3C' }
-    }
+            '.label': {text: kk_conf_confeditor_plugins_info[id].name, 'ref-x': .4, 'ref-y': .2, 'font-size': 12},
+            rect: {fill: '#2ECC71'}
+        }
     });
+    rect.id=id;
     graph.addCells([rect]);
 
     return rect;
 }
 
-function AddDiagramLink(Rid,PinIn,Rid2,PinOut)
+function AddDiagramLink(Rid, Rid2, LinkName)
 {
     var link = new joint.dia.Link({
-    source: { id: Rid, port: PinIn },
-    target: { id: Rid2, port: PinOut }
+        source: {id: Rid},
+        target: {id: Rid2}
     });
+
+
+    link.label(0, {
+        position: 0.5,
+        attrs: {
+            rect: {fill: 'white'},
+            text: {fill: 'blue', text: LinkName}
+        }
+    });
+
+    link.on('change:source', function () {
+        SetLinkType();
+    })
     graph.addCells([link]);
 }
 
@@ -155,4 +187,143 @@ function InitDiagram()
         model: graph,
         gridSize: 1
     });
+
+    var myAdjustVertices = _.partial(adjustVertices, graph);
+
+// adjust vertices when a cell is removed or its source/target was changed
+    graph.on('add remove change:source change:target', myAdjustVertices);
+
+// also when an user stops interacting with an element.
+    paper.on('cell:pointerup', myAdjustVertices);
+
+    paper.on("cell:pointerdblclick", function (cellView, evt, x, y) {
+        if (!cellView.model.isLink())
+        {
+            OpenPluginEditor(cellView.model.id);
+        }
+
+    });
+
 }
+
+
+function OpenPluginEditor(id)
+{
+    var Plugin=kk_conf_confeditor_plugins_info[id];
+    //Show Editor
+    p = $(".popup_overlay");
+    p.css('display', 'block')
+    //Fill
+    //Head
+    $(kk_conf_plugineditor_pname).text(Plugin.name);
+    $(kk_conf_plugineditor_pdesc).text(Plugin.description);
+    //Connections
+    var CurrentFeature = kk_conf_configurations_obj[kk_conf_configuration_mainconf].Features[kk_conf_configuration_currentfeature];
+    //
+    for (var i=0;i<CurrentFeature.Connections.length;i++)
+    {
+        $("#kk_config_plugineditor_connections").add($("#kk_config_plugineditor_connection").clone().attr('id',"#kk_config_plugineditor_connection_"+i));
+         $("#kk_config_plugineditor_connection_"+i)
+    }
+    
+}
+
+function GetPluginConnections(id)
+{
+    
+    
+    
+}
+function CloseLinkProperty()
+{
+    p = $(".popup_overlay");
+    p.css('display', 'none')
+}
+
+
+function adjustVertices(graph, cell) {
+
+    // If the cell is a view, find its model.
+    cell = cell.model || cell;
+
+    if (cell instanceof joint.dia.Element) {
+
+        _.chain(graph.getConnectedLinks(cell)).groupBy(function (link) {
+            // the key of the group is the model id of the link's source or target, but not our cell id.
+            return _.omit([link.get('source').id, link.get('target').id], cell.id)[0];
+        }).each(function (group, key) {
+            // If the member of the group has both source and target model adjust vertices.
+            if (key !== 'undefined')
+                adjustVertices(graph, _.first(group));
+        });
+
+        return;
+    }
+
+    // The cell is a link. Let's find its source and target models.
+    var srcId = cell.get('source').id || cell.previous('source').id;
+    var trgId = cell.get('target').id || cell.previous('target').id;
+
+    // If one of the ends is not a model, the link has no siblings.
+    if (!srcId || !trgId)
+        return;
+
+    var siblings = _.filter(graph.getLinks(), function (sibling) {
+
+        var _srcId = sibling.get('source').id;
+        var _trgId = sibling.get('target').id;
+
+        return (_srcId === srcId && _trgId === trgId) || (_srcId === trgId && _trgId === srcId);
+    });
+
+    switch (siblings.length) {
+
+        case 0:
+            // The link was removed and had no siblings.
+            break;
+
+        case 1:
+            // There is only one link between the source and target. No vertices needed.
+            cell.unset('vertices');
+            break;
+
+        default:
+
+            // There is more than one siblings. We need to create vertices.
+
+            // First of all we'll find the middle point of the link.
+            var srcCenter = graph.getCell(srcId).getBBox().center();
+            var trgCenter = graph.getCell(trgId).getBBox().center();
+            var midPoint = g.line(srcCenter, trgCenter).midpoint();
+
+            // Then find the angle it forms.
+            var theta = srcCenter.theta(trgCenter);
+
+            // This is the maximum distance between links
+            var gap = 20;
+
+            _.each(siblings, function (sibling, index) {
+
+                // We want the offset values to be calculated as follows 0, 20, 20, 40, 40, 60, 60 ..
+                var offset = gap * Math.ceil(index / 2);
+
+                // Now we need the vertices to be placed at points which are 'offset' pixels distant
+                // from the first link and forms a perpendicular angle to it. And as index goes up
+                // alternate left and right.
+                //
+                //  ^  odd indexes 
+                //  |
+                //  |---->  index 0 line (straight line between a source center and a target center.
+                //  |
+                //  v  even indexes
+                var sign = index % 2 ? 1 : -1;
+                var angle = g.toRad(theta + sign * 90);
+
+                // We found the vertex.
+                var vertex = g.point.fromPolar(offset, angle, midPoint);
+
+                sibling.set('vertices', [{x: vertex.x, y: vertex.y}]);
+            });
+    }
+}
+;
